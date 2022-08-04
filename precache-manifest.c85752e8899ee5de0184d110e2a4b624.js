eefeb26054a385da9881b07801a6c46f70797a2e
mehaks2 <mehaksharma8b@gmail.com>
2022-08-04 10:59:03 +0530

    Updates

---
+++ precache-manifest.c85752e8899ee5de0184d110e2a4b624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e56480bccff86b06c4e4",
+    "url": "/highcourt.github.io/static/css/main.8d9ed34c.chunk.css"
+  },
+  {
+    "revision": "e56480bccff86b06c4e4",
+    "url": "/highcourt.github.io/static/js/main.e56480bc.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/highcourt.github.io/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "7fa91f9deba5a26d2bea",
+    "url": "/highcourt.github.io/static/css/2.485234bc.chunk.css"
+  },
+  {
+    "revision": "7fa91f9deba5a26d2bea",
+    "url": "/highcourt.github.io/static/js/2.7fa91f9d.chunk.js"
+  },
+  {
+    "revision": "9a5613c21a417d15565b",
+    "url": "/highcourt.github.io/static/js/runtime~main.9a5613c2.js"
+  },
+  {
+    "revision": "f2810b24ff5b10220b8dab990b4630c3",
+    "url": "/highcourt.github.io/index.html"
+  }
+];
