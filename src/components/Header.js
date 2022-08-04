0803457b692638c9aee1a94c829de5b97d0016dc
mehaks2 <mehaksharma8b@gmail.com>
2022-08-04 10:38:25 +0530

    High court react app on gihub pages

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+	const Header=()=>{
+		
+		return(
+			<>
+				<div className='row'>
+					<div className='col-md-3 col-3'>
+						<div className='logo'>
+							<a href="" title="Home" rel="home" className="header__logo" id="logo">
+								<img className="national_emblem" src="/GoI-logo.svg" alt="national emblem" />
+								HIGH COURT OF <span>Punjab and Haryana</span>
+							</a>
+						</div>
+					</div>
+					<div className='col-md-6 col-6 text-center align-text-bottom middleText' >
+						"It is better to risk saving a guilty person than to condemn an innocent one." - Voltaire Zadig
+					</div>
+					<div className="col-md-3 col-3">
+						<ul className="nav float-right">					
+							<li className="nav-item d-none d-sm-block d-lg-block">
+								<a className="nav-link my-4 mx-0 mx-lg-1 " id="screenRead" href="#" data-toggle="modal" data-target="#screenReaderBoard">Screen Reader </a>
+							</li>
+							<li className="nav-item d-none d-sm-block d-lg-block">
+								<a id="btn-decrease" className="btn btn-black btn-circle btn-sm  btn-sm my-4 mx-0 mx-lg-1" href="#">A-</a>
+							</li>
+							<li className="nav-item d-none d-sm-block d-lg-block">
+								<a id="btn-orig" className="btn btn-black btn-circle btn-sm  btn-sm my-4 mx-0 mx-lg-1" href="#">A</a>
+							</li>
+							<li className="nav-item d-none d-sm-block d-lg-block">
+								<a id="btn-increase" className="btn btn-black btn-circle btn-sm  btn-sm my-4 mx-0 mx-lg-1" href="#">A+</a>
+							</li>
+							<li className="nav-item  d-sm-block d-lg-block">
+								<a id="btn-light" className="btn btn-light btn-outline-dark  btn-sm  btn-sm my-4 mx-0 mx-lg-1" href="#" onclick="change_contrast('light')">A</a>
+							</li>
+							<li className="nav-item  d-sm-block d-lg-block">
+								<a id="btn-dark" className="btn btn-black  btn-sm  btn-sm my-4 mx-0 mx-lg-1" href="#" onclick="change_contrast('dark')">A</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</>
+		);
+	}
+	export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+
+const Navbar=()=>{
+	
+	return(
+
+		<>
+			<nav className="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
+				
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+				  <span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="collapsibleNavbar">					
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a className="nav-link" href="#" >Home</a>
+						</li>
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">About High Court</a>
+							<ul className="dropdown-menu">
+							  <li><a className="dropdown-item" href="#">History</a></li>
+							  <li><a className="dropdown-item" href="#">Building/Architecture</a></li>
+							  <li><a className="dropdown-item" href="#">Court etiquette</a></li>
+							  <li><a className="dropdown-item" href="#">Photo Gallery</a></li>
+							  <li><a className="dropdown-item" href="#">Jurisdiction</a></li>
+							  <li><a className="dropdown-item" href="#">Events</a></li>
+							  <li><a className="dropdown-item" href="#">Calendar</a></li>
+							</ul>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Honable Judges</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Administration</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Case Information</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">District Judiciary</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+}
+
+export default Navbar;
